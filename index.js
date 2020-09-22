f912c73740b3909f78b994f347977b7639b77279
paulmorales7 <67281785+paulmorales7@users.noreply.github.com>
2020-09-22 17:33:41 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+// array of questions for user
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs");
+const Choices = require("inquirer/lib/objects/choices");
+
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "What is your project description?"
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Do you have intallation instructions? If not respond with none."
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "What is your project usage?"
+    },
+    {
+        type: "checkbox",
+        message: "Please select your license choice?",
+        choices: [
+            "apache",
+            "MIT",
+            "Boost",
+            "BSD"
+        ],
+        name: "License",
+    },
+    {
+        type: "input",
+        name: "Contributing",
+        message: "Who contributed on this project?"
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "Do you have any test instructions? If not respond with none."
+    },
+    {
+        type: "input",
+        name: "Github",
+        message: "What is your Github user name?"
+    },
+    {
+        type: "input",
+        name: "Email",
+        message: "What is your Email?"
+    }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
+        if (err) {
+            throw err
+        }
+    })
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions).then(function (answers) {
+
+        const readMe = generateMarkdown(answers);
+        console.log(readMe)
+        writeToFile('readMe.md', readMe)
+    })
+}
+
+// function call to initialize program
+init();
---
+++ readMe.md
@@ -0,0 +1,31 @@
+
+# ReadMe Generator
+
+# Table of Contents
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+# Description
+![License](https://img.shields.io/badge/License-apache-blue.svg "License Badge")
+ - This app will help you build a professional readme for any project you are working on through prompts the user answers
+# Installation
+none
+# Usage
+This app is to be used a template for building the perfect readme
+# License
+License selected displayed in description.
+apache
+## Contributing
+I solely worked on this with the starter code provided by trilogy
+# Tests
+none
+# Questions
+For any questions please review the project in Github or feel free to contact to me.
+- Github: paulmorales7
+- Email: paulginga@hotmail.com
